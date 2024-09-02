28f04551fbe9a07e052199fa60759ccaa12ff025
Brandon Perfetti <brandon@brandonperfetti.com>
2024-09-02 14:41:09 -0700

    Complete Clerk Email Authentication

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -4,10 +4,7 @@ import "react-native-reanimated";
 const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 };
